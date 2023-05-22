1cf40afd7b0b70741346212abb01a49b908350ae
thxssio <thassiogomes44@gmail.com>
2023-05-22 15:48:59 -0300

    Add Angle

--- python/MPU_6050/test.py
+++ python/MPU_6050/test.py
@@ -2,6 +2,8 @@ from mpu6050 import mpu6050
 from time import sleep
 import sys
 import os
+import math
+from math import sqrt, atan
 
 sensor = mpu6050(0x68)
 
@@ -9,7 +11,24 @@ while True:
     accel_data = sensor.get_accel_data()
     gyro_data = sensor.get_gyro_data()
     temp = sensor.get_temp()
-
+    
+    RateRoll=gyro_data['x']/65.5
+    RatePitch=gyro_data['y']/65.5
+    RateYaw=gyro_data['z']/65.5
+    
+    AngleRoll = (atan(accel_data['y']/sqrt(accel_data['x']*accel_data['x']+accel_data['z']*accel_data['z'])))*1/(3.142/180)
+    AnglePitch = (atan(accel_data['x']/sqrt(accel_data['y']*accel_data['y']+accel_data['z']*accel_data['z'])))*1/(3.142/180)
+    
+    
+    
+    print("Rate Roll", RateRoll)
+    print("Rate Pitch", RatePitch)
+    print("Rate Yaw", RateYaw)
+    print("")
+    print("AngleRoll", AngleRoll)
+    print("AnglePitch", AnglePitch)
+    #print("AngleYaw", AngleYaw)
+    """
     print("Accelerometer data")
     print("x: " + str(accel_data['x']))
     print("y: " + str(accel_data['y']))
@@ -21,5 +40,9 @@ while True:
     print("z: " + str(gyro_data['z']))
 
     print("Temp: " + str(temp) + " C")
+    """
+    
+    
     sleep(0.1)
     os.system('clear')
+    
