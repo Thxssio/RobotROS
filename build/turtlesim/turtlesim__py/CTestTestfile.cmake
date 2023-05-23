11660fc6bcb84243bbd09cbc5a4061efd51a03ed
thxssio <thassiogomes44@gmail.com>
2023-05-22 21:33:42 -0300

    v1.0

---
+++ build/turtlesim/turtlesim__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/thxssio/Codes/RobotROS/build/turtlesim/turtlesim__py
+# Build directory: /home/thxssio/Codes/RobotROS/build/turtlesim/turtlesim__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
